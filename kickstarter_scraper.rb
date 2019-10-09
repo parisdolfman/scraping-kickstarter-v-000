79e0e8e9a8df764649c9e3e7f78da3ad2f735df6
Paris Dolfman <parisdolfman@gmail.com>
2019-10-09 20:00:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,11 +1,11 @@
 require "nokogiri"
-
+require "pry"
 
 def create_project_hash
 html =
 File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-
+  brinding.pry
 end
 
 create_project_hash
